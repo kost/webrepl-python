0c48a0a9376a41be4fed7bce3db8d71daa73c5ce
Vlatko Kosturjak <kost@linux.hr>
2019-12-19 17:20:29 +0100

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="webrepl",
-    version="0.1.0",
+    version="0.2.0",
     author="Vlatko Kosturjak",
     author_email="vlatko.kosturjak@gmail.com",
     description="Handle micropython web_repl",
